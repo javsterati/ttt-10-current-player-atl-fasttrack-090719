555b73ae494cd6985cd1ebef5aac15cf98fd13c6
Javier Huerta Jr <javsteratii@gmail.com>
2019-09-08 18:38:05 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,14 +5,14 @@ def turn_count(board)
       turns += 1
     end
   end
-  return turns
+  puts turns
 end
 
 def current_player(board)
   turns = turn_count(board)
-  if turns % 2 == 0
-    return "X"
+  if turns / 2 == 0
+    puts "X"
   else
-    return "O"
+    puts "O"
   end
 end
